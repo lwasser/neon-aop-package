91cfb9de5d4edf11b1a3468a2291e5d298c22526
lwasser <lwasser@neoninc.org>
2016-05-11 14:48:58 -0700

    updating build r script

--- run_package.R
+++ run_package.R
@@ -4,3 +4,7 @@ setwd("~/Documents/GitHub/neon-aop-package/neonAOP")
 document()
 setwd("~/Documents/GitHub/neon-aop-package")
 install("neonAOP")
+
+
+## install from github
+install_github("lwasser/neon-aop-package/neonAOP")
